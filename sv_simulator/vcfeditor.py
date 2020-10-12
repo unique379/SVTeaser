03ab4d82914d48d58b78f731890e3d9204fbec37
Joyjit Daw <jdaw@nvidia.com>
2020-10-12 00:01:39 -0400

    [scripts] fix python env

--- sv_simulator/vcfeditor.py
+++ sv_simulator/vcfeditor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ######################################
 #                                    #
 # Author : Joyjit Daw                #
--- test/vcfeditor/run.sh
+++ test/vcfeditor/run.sh
@@ -4,5 +4,6 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+rm $SCRIPT_DIR/simulated.*
 SURVIVOR simSV $SCRIPT_DIR/ref.fa $SCRIPT_DIR/parameter_file 0.0 0 $SCRIPT_DIR/simulated
-python $SCRIPT_DIR/../../sv_simulator/vcfeditor.py -r $SCRIPT_DIR/ref.fa -i $SCRIPT_DIR/simulated.insertions.fa -v $SCRIPT_DIR/simulated.vcf -o $SCRIPT_DIR/out.vcf
+python3 $SCRIPT_DIR/../../sv_simulator/vcfeditor.py -r $SCRIPT_DIR/ref.fa -i $SCRIPT_DIR/simulated.insertions.fa -v $SCRIPT_DIR/simulated.vcf -o $SCRIPT_DIR/out.vcf
