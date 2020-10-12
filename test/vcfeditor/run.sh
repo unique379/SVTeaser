bc4e4d69fb342bc4318839e7f13d249d5ea0a6e5
Joyjit Daw <jdaw@nvidia.com>
2020-10-11 23:34:28 -0400

    [test] add test data for vcfeditor
    1. run.sh script with sample command
    2. reference fasta and output of running SURVIVOR

---
+++ test/vcfeditor/run.sh
@@ -0,0 +1,4 @@
+# Example run command for SURVIVOR to generate test data
+# Please have SURVIVOR installed and in the $PATH var prior to running this script.
+
+./SURVIVOR simSV ref.fa ./parameter_file 0.0 0 simulated
